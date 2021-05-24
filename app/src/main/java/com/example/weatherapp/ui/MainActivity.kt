5b06797ab95f12dda9ac3c84b25e1eb57dedadc7
Apoorv Srivastava <apoorvsrivastava.4123@gmail.com>
2021-05-24 18:51:11 +0530

    add ui part

--- app/src/main/java/com/example/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/ui/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.weatherapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.weatherapp.R
+import com.example.weatherapp.ui.home.HomeFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val homeFragment = HomeFragment()
+    private val fragmentManager = supportFragmentManager
+    private var activeFragment: Fragment = homeFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fragmentManager.beginTransaction().apply {
+            add(R.id.container, homeFragment, getString(R.string.home_dashboard))
+        }.commit()
+        initListeners()
+    }
+
+    private fun initListeners() {
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.homeFragment -> {
+                    fragmentManager.beginTransaction().hide(activeFragment).show(homeFragment)
+                        .commit()
+                    activeFragment = homeFragment
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
 }
